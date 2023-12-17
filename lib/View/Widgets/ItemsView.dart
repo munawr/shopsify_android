8c603c41c5ad616dd23d2af84ed146bead2ddf18
Munawar <munawart4tirur@gmail.com>
2023-12-17 23:40:22 +0530

    List of Items rows

--- lib/View/Widgets/ItemsView.dart
+++ lib/View/Widgets/ItemsView.dart
@@ -1,118 +1,121 @@
 import 'package:flutter/material.dart';
 
-class Items extends StatelessWidget {
-  const Items({super.key});
+
+class CustomItems extends StatelessWidget {
+  String? image;
+  String? offer;
+  String? bestSeller;
+  String? dealOfTheDay;
+  String? name;
+   CustomItems({super.key, this.image,this.bestSeller,this.dealOfTheDay,this.name,this.offer});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
       color: Colors.grey,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            color: Colors.white,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Stack(
-                  children: [
-                    Image.network(
-                      'https://images.unsplash.com/photo-1585123334904-845d60e97b29?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2F0Y2hlc3xlbnwwfHwwfHx8MA%3D%3D',
-                      width: double.infinity,
-                      height: 130.0,
-                      fit: BoxFit.cover,
-                    ),
-                    Column(
-                      children: [
-                        Row(
-                          children: [
-                            Positioned(
-                              top: 0,
-                              right: 0,
-                              child: Container(
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: 8.0, vertical: 4.0),
-                                color: Color(0xFF0298b1),
-                                child: Text(
-                                  'Best Seller',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Spacer(),
-                            IconButton(
-                              icon: Icon(Icons.favorite),
-                              color: Colors.grey,
-                              onPressed: () {
-                                // Handle like button press
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: Column(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Image.network(
+                    '$image',
+                    width: double.infinity,
+                    height: 130.0,
+                    fit: BoxFit.cover,
+                  ),
+                  Column(
                     children: [
-                      Container(
-                        height: 50,
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 20,
-                              color: Colors.red,
+                      Row(
+                        children: [
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 8.0, vertical: 4.0),
+                              color: Color(0xFF0298b1),
                               child: Text(
-                                '50% off!',
+                                '$bestSeller',
                                 style: TextStyle(
                                   color: Colors.white,
-                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
-                            ),SizedBox(width: 8,),
-                            Expanded(
-                              child: Text(
-                                'Deal of the Day',
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  color: Colors.red,
-                                  fontSize: 14.0,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          height: 50,
-                          child: Text(
-                            'HAMILTON FMH606K Stainless Steel ',
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontSize: 14.0,
-                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
+                          Spacer(),
+                          IconButton(
+                            icon: Icon(Icons.favorite),
+                            color: Colors.grey,
+                            onPressed: () {
+                              // Handle like button press
+                            },
+                          ),
+                        ],
                       ),
                     ],
                   ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Container(
+                      height: 50,
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 20,
+                            color: Colors.red,
+                            child: Text(
+                              '$offer',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),SizedBox(width: 8,),
+                          Expanded(
+                            child: Text(
+                              'Deal of the Day',
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 14.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 50,
+                        child: Text(
+                          '$name ',
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/View/home_screen.dart
+++ lib/View/home_screen.dart
@@ -15,19 +15,42 @@ class HomeScreen extends StatelessWidget {
       create: (context) => HomeScreenProvider(),
       child: Consumer<HomeScreenProvider>(builder: (context, data, _) {
         return Scaffold(
+          backgroundColor: Colors.grey,
           appBar: CustomAppBar(),
-          body: GridView.count(
-            crossAxisCount: 2, // Two columns
-            childAspectRatio: 0.7, // Adjust height to width ratio
-            mainAxisSpacing: 10, // Spacing between rows
-            crossAxisSpacing: 10, // Spacing between columns
-            padding: const EdgeInsets.all(10), // Padding around the grid
-            children: List.generate(6, (index) {
+          body:
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "POLICE Analog Watch - For Men PLPEWJF0006203",image: "https://rukminim2.flixcart.com/image/612/612/xif0q/watch/r/l/t/1-plpewjf0006203-police-men-original-imaguh42zg64xvas.jpeg?q=70",offer: "40% off%",),
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Fastrack Tees Analog Watch - For Men 68011PP08",image: "https://rukminim2.flixcart.com/image/416/416/xif0q/watch/a/k/e/-original-imagnydrewxe5qeg.jpeg?q=70",offer: "50% off%",),
+                  ],
+                ),
+                Row(
+                  children: [
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Titan Karishma refresher Analog Watch - For Men NM1712YM02",image: "https://rukminim2.flixcart.com/image/832/832/k79dd3k0pkrrdj/watch-refurbished/p/g/j/d-1712ym02-titan-original-imafpkmqkc3rhstx.jpeg?q=70",offer: "60% off%",),
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Fastrack Stunners 1.0 Analog Watch - For Men 3254NM01",image: "https://rukminim2.flixcart.com/image/832/832/xif0q/watch/p/9/q/-original-imagnvwqkhhs7bfy.jpeg?q=70",offer: "20% off%",),
+                  ],
+                ),
 
-              //final product = products[index];
-              return Items(); // Build your product card widget
-            }),
-          ));
+                Row(
+                  children: [
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Fastrack Tees Analog Watch - For Men 68011PP08",image: "https://rukminim2.flixcart.com/image/832/832/xif0q/watch/p/9/q/-original-imagnvwqkhhs7bfy.jpeg?q=70",offer: "30% off%",),
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Fastrack Minimalists Analog Watch - For Men 38024PP25",image: "https://rukminim2.flixcart.com/image/832/832/xif0q/watch/v/w/o/-original-imagta7uyupbfzsf.jpeg?q=70",offer: "50% off%",),
+                  ],
+                ),
+                Row(
+                  children: [
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Titan Analog Watch - For Men NP1773SL02",image: "https://rukminim2.flixcart.com/image/832/832/xif0q/watch/x/o/8/-original-imagnvwtyv2panhd.jpeg?q=70",offer: "63% off%",),
+                    CustomItems(bestSeller: "Best Seller", dealOfTheDay: "Deal of the Day",name: "Fastrack Tees Analog Watch - For Men 68011PP08",image: "https://rukminim2.flixcart.com/image/416/416/xif0q/watch/a/k/e/-original-imagnydrewxe5qeg.jpeg?q=70",offer: "700% off%",),
+                  ],
+                ),
+              ],
+            ),
+          ),
+
+        );
       }),
     );
   }
